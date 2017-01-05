61b8510d0887772411df1593541961db4a5359ff
Даниил Ларионов < >
2017-01-06 01:18:42 +0300

    Initial commit

---
+++ src/main/java/xyz/rexhaif/classifycat/Launcher.java
@@ -0,0 +1,14 @@
+package xyz.rexhaif.classifycat;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Launcher {
+    private static Logger LOG = LoggerFactory.getLogger("Launcher");
+
+    public static void main(String[] args) {
+        LOG.info("It works!");
+    }
+
+}
